45c48972a8ecad90c24298dc8938c2789bae2dce
aayush-sarvaiya <71958357+aayush-sarvaiya@users.noreply.github.com>
2026-04-19 10:58:37 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'trade-journal-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        './trading-journal.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
